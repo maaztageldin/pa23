cef9569c0a01e8b0c39ee2eff558c40e09d2bd25
tageldin <maaztageldin030@hotmail.com>
2023-07-17 19:37:38 +0200

    firebase confige etat final

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
-//import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const LEDControlApp());
 }
 
 class LEDControlApp extends StatelessWidget {
   const LEDControlApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     //DatabaseReference ref = FirebaseDatabase.instance.ref("users/123");
@@ -55,56 +57,62 @@ class HomeScreen extends StatelessWidget {
             ElevatedButton(
               child: const Text('M1'),
               onPressed: () {
-                allumerLED('M1');
+                updateMotifValue('M1');
               },
             ),
             ElevatedButton(
               child: const Text('M2'),
               onPressed: () {
-                allumerLED('M2');
+                updateMotifValue('M2');
               },
             ),
             ElevatedButton(
               child: const Text('M3'),
               onPressed: () {
-                allumerLED('M3');
+                updateMotifValue('M3');
               },
             ),
             ElevatedButton(
               child: const Text('M4'),
               onPressed: () {
-                allumerLED('M4');
+                updateMotifValue('M4');
               },
             ),
             ElevatedButton(
               child: const Text('M5'),
               onPressed: () {
-                allumerLED('M5');
+                updateMotifValue('M5');
               },
             ),
             const SizedBox(height: 20),
-            ElevatedButton(
-              child: const Text('Allumer/Éteindre'),
-              onPressed: () {
-                allumerEteindreLED();
-              },
+
+            Padding(
+
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  ElevatedButton(
+                    child: const Text('Allumer'),
+                    onPressed: () {
+                      allumerEteindreLED('ON');
+                    },
+                  ),
+                  ElevatedButton(
+                    child: const Text('Éteindre'),
+                    onPressed: () {
+                      allumerEteindreLED('OFF');
+                    },
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
     );
   }
-
-  void allumerLED(String motif) {
-    // Fonction pour allumer les LED en fonction du motif sélectionné
-    print('Allumer les LED avec le motif $motif');
-  }
-
-  void allumerEteindreLED() {
-    // Fonction pour allumer ou éteindre les LED
-    print('Allumer/éteindre les LED');
-  }
 }
+
 
 class AjouterItineraireScreen extends StatelessWidget {
   const AjouterItineraireScreen({super.key});
@@ -121,3 +129,22 @@ class AjouterItineraireScreen extends StatelessWidget {
     );
   }
 }
+
+void updateMotifValue(String motif) {
+  DatabaseReference databaseReference = FirebaseDatabase.instance.ref().child('LED_MOTIF');
+  databaseReference.set(motif).then((value) {
+    print('Valeur mise à jour avec succès');
+  }).catchError((error) {
+    print('Erreur lors de la mise à jour de la valeur : $error');
+  });
+}
+
+void allumerEteindreLED(String motif) {
+  DatabaseReference databaseReference = FirebaseDatabase.instance.ref().child('LED_STATUS');
+  databaseReference.set(motif).then((value) {
+    print('Valeur mise à jour avec succès');
+  }).catchError((error) {
+    print('Erreur lors de la mise à jour de la valeur : $error');
+  });
+}
+
